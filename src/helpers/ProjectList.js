a9e5916fac43d0045ea314cb5f1fe02d05c2fb88
Jaejin Kim <jaejink@uci.edu>
2023-03-28 02:48:35 -0700

    projects linked into each one's separate page

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,15 @@
+import Proj1 from "../assets/proj1.jpg";
+import Proj2 from "../assets/proj2.jpg";
+
+export const projectList = [
+    {
+        name: "Alorithm Visualizer",
+        image: Proj1,
+        skills: "React"
+    },
+    {
+        name: "Spotify Clone",
+        image: Proj2,
+        skills: "React"
+    }
+]
